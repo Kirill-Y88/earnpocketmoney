39bf463d4361065374711cb0b2696f303e201804
KirillYrevichPushkin <benberov09@mail.ru>
2021-12-09 23:36:24 +0300

    Backendstep2 commit #2 Реализованы CRUD методя для моделей

--- src/main/java/ru/coolteam/earnpocketmoney/services/TaskService.java
+++ src/main/java/ru/coolteam/earnpocketmoney/services/TaskService.java
@@ -2,9 +2,12 @@ package ru.coolteam.earnpocketmoney.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.coolteam.earnpocketmoney.models.Child;
+import ru.coolteam.earnpocketmoney.models.Parent;
 import ru.coolteam.earnpocketmoney.models.Task;
 import ru.coolteam.earnpocketmoney.repositories.TaskRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,7 +16,7 @@ import java.util.Optional;
 public class TaskService {
     private final TaskRepository taskRepository;
 
-    public Optional<Task> findById(Long id) {
+    public Optional<Task> findById(Integer id) {
         return taskRepository.findById(id);
     }
 
@@ -21,10 +24,30 @@ public class TaskService {
         return taskRepository.findAll();
     }
 
-    public Task createTask(String title) {
+    public Task createTask(String title, Parent parent, Child child, Integer cost) {
         Task task = new Task();
         task.setTitle(title);
-        task = taskRepository.save(task);
-        return task;
+        task.setParent(parent);
+        task.setChild(child);
+        task.setCost(cost);
+        return taskRepository.save(task);
     }
+
+    public Optional<Task> findByTitle (String title) {
+        return taskRepository.findTaskByTitle(title);
+    }
+
+    public Task updatedTime (String title, LocalDateTime updatedAt){
+        Task task = taskRepository.findTaskByTitle(title).get();
+        task.setUpdatedAt(updatedAt);
+        return taskRepository.save(task);
+    }
+
+    public boolean delete (String title){
+        Task task = taskRepository.findTaskByTitle(title).get();
+        taskRepository.delete(task);
+        return true;
+    }
+
+
 }
